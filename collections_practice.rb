2a8ea329d7d178eb2a818a9d4cf8ebca8587ff9f
Kay B <kaylouisebennett@gmail.com>
2018-08-22 22:58:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
 # your code goes here
+
+def begins_with_r(array)
+array.all? { |r| r[0] == "r" }
+end
+
+def contain_a(array)
+array.select { |letter| letter.include?("a") }
+end
+
+def first_wa(array)
+  array.detect { |word| word.to_s.start_with?("wa") }
+end
+
+def remove_non_strings(array)
+  array.delete_if { |array| !(array.is_a? String) }
+  end
+  
+  def count_elements(array)
+  array.count { |elements| elements > 0 }
+  end
